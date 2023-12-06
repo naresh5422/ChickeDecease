6c8fcae3309e14871660c411baac9b34fd891749
naresh <naresh09022cm023@gmail.com>
2023-12-06 12:16:15 +0530

    Update the stage_01 files

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from CNNClassifier import logger
+from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome Custom Logs")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} is completed <<<<<<<<\n\nX========X")
+except Exception as e:
+    logger.exception(e)
+    raise e

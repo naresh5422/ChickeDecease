b9c6888fcf0efa9030a02b8ed7949446adbd3bf4
naresh <naresh09022cm023@gmail.com>
2023-12-06 17:17:50 +0530

    Added Prepare base model

--- src/CNNClassifier/constants/__init__.py
+++ src/CNNClassifier/constants/__init__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
 # While params.yaml is empty it will return error
-# So mention eny thing inside params.yaml file
+# So mention anything inside params.yaml file
 PARAMS_FILE_PATH = Path("params.yaml")
